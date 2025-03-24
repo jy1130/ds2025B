c756cd00ae2cc53df5939714acf09c694d6ef4fb
wldb <daily476@naver.com>
2025-03-24 11:55:15 +0900

    v0.9 array

--- week03.py
+++ week03.py
@@ -1,4 +1,19 @@
-city = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
-#city = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
-city = set(city)
-print(city)
+def duplicate_city(cities):
+    result = list()
+    s=set()
+
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2:#길이가 같다면 중복된 값이 들어갔다는 뜻
+            result.append(city)
+    return result
+
+
+cities = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
+#cities = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
+cities.append('Anyang')
+cities.append('Seoul')
+print(cities)
+print(set(duplicate_city(cities))
