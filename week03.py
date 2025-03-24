a551a605cb488ea7b448330f3be6e95c03811cf9
wldb <daily476@naver.com>
2025-03-24 10:51:19 +0900

    v0.5 array

--- week03.py
+++ week03.py
@@ -1,7 +1,5 @@
-array = [11, 9, -77, 8]
-fir i in range(len(array)):
-    print(f"{array[i]:3} {id(array[I])}")
-print(f"{array[0]:3}{id(array[0])}")
-print(f"{array[1]:3}{id(array[1])}")
-print(f"{array[2]:3}{id(array[2])}")
-print(f"{array[3]:3}{id(array[3])}")
+import array
+arr = array.array('f',[11, 9, -77, 8])
+for i in range(len(arr)):
+    print(f"{arr[i]:3} {id(arr[i])}")
+print(arr[2])
