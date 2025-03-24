16ec7a4670ff9d629ac0de6763bd6d2706b9ac46
wldb <daily476@naver.com>
2025-03-24 11:44:42 +0900

    v0.8 array

--- week03.py
+++ week03.py
@@ -1,6 +1,4 @@
-groups = ['HOT', 'Seventeen', 'Black pink', 'NJZ']
-ratings = [1, 2, 4 ,3]
-#짝이 맞아야 모든 함수가 출력될 수 있다.
-
-group_ratings = list(zip(groups, ratings))
-print(group_ratings)
+city = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
+#city = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
+city = set(city)
+print(city)
