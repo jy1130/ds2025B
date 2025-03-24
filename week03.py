5ee21c1606a07bdd34144267a0a6358feb86afeb
wldb <daily476@naver.com>
2025-03-24 10:45:30 +0900

    v0.4 array

---
+++ week03.py
@@ -0,0 +1,7 @@
+array = [11, 9, -77, 8]
+fir i in range(len(array)):
+    print(f"{array[i]:3} {id(array[I])}")
+print(f"{array[0]:3}{id(array[0])}")
+print(f"{array[1]:3}{id(array[1])}")
+print(f"{array[2]:3}{id(array[2])}")
+print(f"{array[3]:3}{id(array[3])}")
